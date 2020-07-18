ed9d86d16aedb536161dbc103c54de304fce6a38
studyerer <2429831566@qq.com>
2020-07-18 14:53:08 +0800

    方便打jar包

--- src/main/java/com/blog/sunyu/SunYuApplication.java
+++ src/main/java/com/blog/sunyu/SunYuApplication.java
@@ -3,10 +3,19 @@ package com.blog.sunyu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("com.blog.sunyu.dao")
 @SpringBootApplication
-public class SunYuApplication {
+public class SunYuApplication
+//        extends SpringBootServletInitializer
+{
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(SunYuApplication.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(SunYuApplication.class, args);
